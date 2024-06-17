e63afa68b392c206ae91e0a1c3648c539e55c1bc
Bennet <bennet.21072@mhs.unesa.ac.id>
2024-06-17 19:04:11 +0700

    first commit

---
+++ omahputih/omahputih/common/config.php
@@ -0,0 +1,63 @@
+<?php
+define('SITE_TITLE', 'OMAH PUTIH GODEAN');
+define('TIME_ZONE', 'Asia/Jakarta');
+define('DATE_FORMAT', '%F');
+define('TIME_FORMAT', '%I:%M%P');
+define('CURRENCY_ENABLED', '1');
+define('CURRENCY_POS', 'before'); // before or after
+define('LANG_ENABLED', '0');
+define('ADMIN_LANG_FILE', 'in.ini');
+define('ENABLE_COOKIES_NOTICE', '1');
+define('MAINTENANCE_MODE', '0');
+define('MAINTENANCE_MSG', '<h1><i class=\"fa fa-rocket\"></i> Coming soon...</h1><p>We are sorry, our website is down for maintenance.</p>');
+define('TEMPLATE', 'default');
+define('OWNER', 'Omah Putih Godean');
+define('EMAIL', 'budihusada@gmail.com');
+define('ADDRESS', 'Jl. Murai No.53, Cokro Gedok, Sidoarum, Daerah Istimewa Yogyakarta, 55564 Ngabean, Indonesia');
+define('PHONE', '087739474000');
+define('MOBILE', '');
+define('FAX', '');
+define('DB_NAME', 'ramandam_oputih');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USER', 'ramandam_roothosting');
+define('DB_PASS', 'r00th0571n6');
+define('SENDER_EMAIL', 'ramdyas@gmail.com');
+define('SENDER_NAME', 'Omah Putih');
+define('USE_SMTP', '1');
+define('SMTP_SECURITY', 'ssl');
+define('SMTP_AUTH', '1');
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USER', 'ramdyas@gmail.com');
+define('SMTP_PASS', 'As!23456');
+define('SMTP_PORT', '465');
+define('GMAPS_API_KEY', '');
+define('ANALYTICS_CODE', '');
+define('ADMIN_FOLDER', 'admin');
+define('CAPTCHA_PKEY', ''); // ReCAPTCHA public key
+define('CAPTCHA_SKEY', ''); // ReCAPTCHA secret key
+define('AUTOGEOLOCATE', '1'); // Change the currency according to the country (https required)
+define('PAYMENT_TYPE', 'arrival'); // 2checkout,paypal,check,arrival
+define('PAYPAL_EMAIL', '');
+define('VENDOR_ID', ''); // 2Checkout.com account number
+define('SECRET_WORD', ''); // 2Checkout.com secret word
+define('PAYMENT_TEST_MODE', '1');
+define('ENABLE_DOWN_PAYMENT', '1');
+define('DOWN_PAYMENT_RATE', '30'); // %
+define('DOWN_PAYMENT_AMOUNT', '50'); // amount required to activate the down payment
+define('ENABLE_TOURIST_TAX', '1');
+define('TOURIST_TAX', '0');
+define('TOURIST_TAX_TYPE', 'fixed');
+define('ALLOW_COMMENTS', '0');
+define('ALLOW_RATINGS', '0'); // If comments enabled
+define('ENABLE_BOOKING_REQUESTS', '1'); // Possibility to make a reservation request if no availability
+define('BRAINTREE_MERCHANT_ID', '');
+define('BRAINTREE_PUBLIC_KEY', '');
+define('BRAINTREE_PRIVATE_KEY', '');
+define('CURRENCY_CONVERTER_KEY', ''); // currencyconverterapi.com API key
+define('RAZORPAY_KEY_ID', '');
+define('RAZORPAY_KEY_SECRET', '');
+define('ENABLE_ICAL', '1');
+define('ENABLE_AUTO_ICAL_SYNC', '1');
+define('ICAL_SYNC_INTERVAL', 'daily'); // daily | hourly
+define('ICAL_SYNC_CLOCK', '7'); // 0-23h mode, required if ICAL_SYNC_INTERVAL = daily
